ded4cff2194ab696e9ee26737a5a2615e253c768
Andres Echeverry <jael2100@gmail.com>
2025-04-14 16:32:42 -0500

    Modificacion del Main.py para key desde admin 2

--- main.py
+++ main.py
@@ -19,7 +19,7 @@ app.add_middleware(
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
-)
+) 
 
 @app.post("/api/generar-imagen")
 async def generar_imagen(request: Request):
